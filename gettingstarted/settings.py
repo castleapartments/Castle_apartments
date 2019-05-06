0c68a13635faec13a7abeaa5499f8511d6591083
Gisli Gudmundsson <gisli@iteggs.com>
2019-05-06 20:20:29 +0000

    test git

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE" : "django.db.backends.sqlite3",
+#        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#    }
+#}
+
 DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Castle_apartments',                      
+        'USER': 'castle',
+        'PASSWORD': 'castle.123',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
