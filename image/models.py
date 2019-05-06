b5ecf36402ba52a968188e94a5e95f2bc78ad6a4
Gisli Gudmundsson <gisli@iteggs.com>
2019-05-06 22:51:45 +0000

    Fixed image adding, can be used for profile and apartments

--- image/models.py
+++ image/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Image(models.Model):
     imageID = models.AutoField(primary_key=True)
-    imageURL = models.ImageField(unique=True)
+    imageURL = models.ImageField(upload_to="media/", blank=True, unique=True)
     imageCreated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.imageURL
+        return str(self.imageURL)
