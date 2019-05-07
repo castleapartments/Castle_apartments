c00a50be5a65dc02f0ba3641733828ddc893cbe0
joninjo <jonij18@ru.is>
2019-05-07 15:18:10 +0000

    Update settings.json

--- apartments/admin.py
+++ apartments/admin.py
@@ -8,3 +8,4 @@ admin.site.register(ApartmentTypeApartment)
 admin.site.register(ApartmentFeature)
 admin.site.register(ApartmentFeatureApartment)
 admin.site.register(ImageApartment)
+#do remove user, updating site settings, admin creations and employee creations also come here as models?
