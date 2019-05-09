685221d1336c8109254b3607ea5fb6207594b2d6
hooper82 <hooper82@gmail.com>
2019-05-09 20:40:12 +0000

    Moving sidenav and topnav to their own files. Fixing url paths.

--- apartments/urls.py
+++ apartments/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list, name='list'),
-    path('list', views.list, name='list'),
-    path('search', views.search, name='search'),
-    path('view', views.view, name='search'),
-    path('add', views.add, name='add'),
+    path('', views.list, name='list_apartments'),
+    path('list', views.list, name='list_apartments'),
+    path('search', views.search, name='search_apartments'),
+    path('view', views.list, name='view_apartment'),
+    path('add', views.add, name='add_apartment'),
 ]
