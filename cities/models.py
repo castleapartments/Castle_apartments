bddc64d777262f49291e250a9b257e4fb35555cf
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2019-05-12 16:35:54 +0000

    Small city fix

--- cities/models.py
+++ cities/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class City(models.Model):
     city_name = models.CharField(max_length=130)
     city_postal_code = models.IntegerField(blank=True)
+    city_zip = models.IntegerField(blank=True)
 
     def __str__(self):
         return str(self.city_name)
